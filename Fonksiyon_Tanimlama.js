92e351ace1b6be85e37670b2112e18cd98512639
JavaScript <63395518+JSWEBTR@users.noreply.github.com>
2020-04-09 13:50:10 +0300

    JavaScript fonksiyon tanımlama yöntemleri.
    JavaScript fonksiyon tanımlama yöntemleri.

---
+++ Fonksiyon_Tanimlama.js
@@ -0,0 +1,15 @@
+// JavaScript fonksiyon tanımlama yöntemleri.
+//www.js.web.tr
+
+//1. Yöntem
+function iyiGunler () {
+    console.log("İyi günler...");
+}
+
+//2. Yöntem
+const iyiAksamlar = function() {
+    console.log("İyi akşamlar...");
+}
+
+//3. Yöntem (arrow fonksiyon)
+const iyiHaftalar = () => {console.log("İyi haftalar...");}
